cfd961c192fbdccb9d20fd4a7ec57bc5c00d551c
ztx <30760655@qq.com>
2024-08-23 15:47:28 +0800

    精简配置

--- N1/diy.sh
+++ N1/diy.sh
@@ -20,28 +20,14 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 #git clone --depth=1 https://github.com/0x676e67/luci-theme-design package/luci-theme-design
 #git clone --depth=1 https://github.com/0x676e67/luci-app-design-config package/luci-app-design-config
-git clone -b v3.35.0 --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone -b v3.35.0 --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 
 #添加自定义的软件包源
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-theme-design
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-design-config
 
-#只能用3.35版本
-#git_sparse_clone master https://github.com/sbwml/luci-app-alist alist
-#git_sparse_clone v3.35.0 https://github.com/sbwml/luci-app-alist luci-app-alist
-
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages softethervpn5
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-softethervpn
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-openvpn-server
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages openvpn-dns-hotplug
-#git_sparse_clone master https://github.com/immortalwrt/packages net/samba4
-#git_sparse_clone master https://github.com/immortalwrt/luci applications/luci-app-samba4
-#git_sparse_clone master https://github.com/immortalwrt/packages net/openvpn
-#git_sparse_clone master https://github.com/immortalwrt/packages net/openvpn-easy-rsa
-#git_sparse_clone master https://github.com/immortalwrt/luci applications/luci-app-openvpn-server
 
 # Remove packages
-#rm -rf package/small-package/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,natflow,wireless-regdb}
 #删除lean库中的插件，使用自定义源中的包。
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/themes/luci-theme-argon
@@ -51,17 +37,6 @@ rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/luci/applications/luci-app-mosdns
 #rm -rf feeds/luci/themes/luci-theme-design
 #rm -rf feeds/luci/applications/luci-app-design-config
-
-#删除默认的softethvpn
-#rm -rf feeds/packages/net/softethervpn5
-#rm -rf feeds/luci/applications/luci-app-softethervpn
-#删除默认的openvpn
-#rm -rf feeds/packages/net/openvpn
-#rm -rf feeds/packages/net/openvpn-easy-rsa
-#rm -rf feeds/luci/applications/luci-app-openvpn-server
-#删除默认的samba4
-#rm -rf feeds/packages/net/samba4
-#rm -rf feeds/luci/applications/luci-app-samba4 luci-app-samba4
 
 # Default IP
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
