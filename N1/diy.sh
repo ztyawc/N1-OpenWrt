267c1c81cfcca53195d0937c9486d9c39a809d90
ztx <30760655@qq.com>
2024-11-06 12:09:22 +0800

    降ddnsgo版本

--- N1/diy.sh
+++ N1/diy.sh
@@ -16,7 +16,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwr
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
-git clone -b v6.7.0 --single-branch --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
+git clone -b v6.6.9 --single-branch --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-lucky package/lucky
 #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
 
