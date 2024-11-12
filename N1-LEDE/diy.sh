bc512dd3b1c39a8b96c64f3426d56917bab6ea5d
ztx <30760655@qq.com>
2024-11-12 11:35:51 +0800

    修改LEDE首页显示的时间格式

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -38,3 +38,6 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 
 # Default IP
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+
+#修改默认时间格式
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
